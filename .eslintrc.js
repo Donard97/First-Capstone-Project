cfc117e2688483a573a56d6998781815ea655745
Donard97 <donardgolaj@gmail.com>
2021-08-10 21:27:05 +0200

    add javasctript linters

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single',
+			'double'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
